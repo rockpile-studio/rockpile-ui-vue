d8839e87ff75e7bfded6daae5eba0d94cd0e8e28
linlei <llei1@linewell.com>
2021-01-13 18:21:42 +0800

    提交更新

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 module.exports = {
+  devServer: {
+    open: false,
+    // host: 'localhost',
+    port: 18080,
+    https: false,
+    proxy: { // 配置跨域
+      '/': {
+        target: 'http://localhost:15030', // 后端服务地址
+        changOrigin: true, // 允许跨域
+        pathRewrite: {
+          '^/': '' // 请求的时候使用这个api就可以
+        }
+      }
+    }
+  },
   configureWebpack: {
     resolve: {
       alias: {}
