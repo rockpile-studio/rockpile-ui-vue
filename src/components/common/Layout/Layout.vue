0b588d0ea81111af19ec812c2bee968440db6bc0
linlei <llei1@linewell.com>
2021-01-13 18:14:06 +0800

    提交更新

--- src/components/common/Layout/Layout.vue
+++ src/components/common/Layout/Layout.vue
@@ -4,10 +4,11 @@
     <div class="app-head">
       <div class="app-head-inner">
         <img src="@/assets/img/logo.png">
-        <!-- 顶部导航-右侧 -->
+        <!-- 顶部 navigation 导航 -右侧 -->
         <div class="head-nav">
           <ul class="nav-list">
-            <li>{{ username }}</li>
+            <li>登陆</li>
+            <li class="nav-pile">|</li>
             <li>注册</li>
             <li class="nav-pile">|</li>
             <li>关于</li>
@@ -15,15 +16,17 @@
         </div>
       </div>
     </div>
+
     <!-- 整体布局：页面内容 -->
     <div class="app-content">
       <keep-alive>
         <router-view></router-view>
       </keep-alive>
     </div>
+
     <!-- 整体布局：页面底部 -->
     <div class="app-foot">
-      <p>©2020 web-app by rockpile studio</p>
+      <p>©2021 Rockpile Studio</p>
     </div>
   </div>
 </template>
